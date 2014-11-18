c86879518e8ec2f37149823279e3d38941944e3f
Thomas Tuegel <ttuegel@gmail.com>
2014-11-17 18:58:28 -0600

    libdbusmenu-qt: add version from the Qt 5 branch

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5853,6 +5853,7 @@ let
   };
 
   libdbusmenu_qt = callPackage ../development/libraries/libdbusmenu-qt { };
+  libdbusmenu_qt5 = callPackage ../development/libraries/libdbusmenu-qt/qt5.nix {};
 
   libdc1394 = callPackage ../development/libraries/libdc1394 { };
 
