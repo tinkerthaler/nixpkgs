d623e63462e7822652e132a1bfa7486a3b223874
Thomas Tuegel <ttuegel@gmail.com>
2014-12-06 16:18:36 -0600

    plasma-desktop: allow overriding mirror in manifest.sh

--- pkgs/desktops/plasma-5.1/manifest.sh
+++ pkgs/desktops/plasma-5.1/manifest.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-# The extra slash at the end of the URL is necessary to stop wget
-# from recursing over the whole server! (No, it's not a bug.)
-../../build-support/autonix/manifest.sh \
-    http://download.kde.org/stable/plasma/5.1.1/ \
-    -A '*.tar.xz'
+if [ $# -eq 0 ]; then
+
+  # The extra slash at the end of the URL is necessary to stop wget
+  # from recursing over the whole server! (No, it's not a bug.)
+  ../../build-support/autonix/manifest.sh \
+      http://download.kde.org/stable/plasma/5.1.1/ \
+      -A '*.tar.xz'
+
+else
+
+  ../../build-support/autonix/manifest.sh -A '*.tar.xz' "$@"
+
+fi
