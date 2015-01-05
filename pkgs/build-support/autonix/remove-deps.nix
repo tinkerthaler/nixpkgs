0026ffd9566dd9f9e8fc91b0ea61c5b9c64d44b7
Thomas Tuegel <ttuegel@gmail.com>
2015-01-05 07:15:02 -0600

    autonix.removeDeps: use fold instead of foldl

--- pkgs/build-support/autonix/remove-deps.nix
+++ pkgs/build-support/autonix/remove-deps.nix
@@ -3,12 +3,12 @@
 with stdenv.lib;
 
 let
-  go = deps: name: mapAttrs (n: v: v // {
+  go = name: mapAttrs (n: v: v // {
     buildInputs = remove name v.buildInputs;
     nativeBuildInputs = remove name v.nativeBuildInputs;
     propagatedBuildInputs = remove name v.propagatedBuildInputs;
     propagatedNativeBuildInputs = remove name v.propagatedNativeBuildInputs;
     propagatedUserEnvPkgs = remove name v.propagatedUserEnvPkgs;
-    }) deps;
+    });
 in
-  foldl go
+  fold go
