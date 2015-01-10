2311a1e1057a1a7058448af731634b8ce0afb7c8
Thomas Tuegel <ttuegel@gmail.com>
2015-01-10 08:29:53 -0600

    autonix: remove remove-deps

--- pkgs/build-support/autonix/remove-deps.nix
+++
@@ -1,9 +0,0 @@
-{ stdenv, isDepAttr }:
-
-with stdenv.lib;
-
-let
-  removeDep = dep:
-    mapAttrs (pkg: mapAttrs (n: if isDepAttr n then remove dep else id));
-in
-deps: pkgs: fold removeDep pkgs deps
