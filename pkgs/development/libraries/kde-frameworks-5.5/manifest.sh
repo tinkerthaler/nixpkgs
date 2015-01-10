af43b2e555c140e4a9e9a491c320f07fd3d906e0
Thomas Tuegel <ttuegel@gmail.com>
2015-01-10 06:50:10 -0600

    kde-frameworks-5.5: may generate manifest from nearby mirror

--- pkgs/development/libraries/kde-frameworks-5.5/manifest.sh
+++ pkgs/development/libraries/kde-frameworks-5.5/manifest.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-# The extra slash at the end of the URL is necessary to stop wget
-# from recursing over the whole server! (No, it's not a bug.)
-../../../build-support/autonix/manifest.sh \
+if [ $# -eq 0 ]; then
+
+  # The extra slash at the end of the URL is necessary to stop wget
+  # from recursing over the whole server! (No, it's not a bug.)
+  ../../../build-support/autonix/manifest.sh \
     http://download.kde.org/stable/frameworks/5.5/ \
-    -A '*.tar.xz'
+      -A '*.tar.xz'
+
+else
+
+  ../../../build-support/autonix/manifest.sh -A '*.tar.xz' "$@"
+
+fi
